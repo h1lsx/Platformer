eeb427342a9a2c57d251e81ddde5e9c0f5d3a6e8
h1lsx <139397050+h1lsx@users.noreply.github.com>
2024-05-22 20:07:35 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "cache-v1";
+const cacheName = "cache-v2";
 const precachedResources = ["index.html", "init.js", "script.js", "edit.js", "classes.js", "tiles.png", "gdicons/ball.png", "gdicons/cubeportal.png", "gdicons/autoportal.png", "gdicons/platformerportal.png", "gdicons/icon.png", "gdicons/jetpackl.png", "gdicons/jetpackr.png", "gdicons/wave.png", "https://cdn.jsdelivr.net/npm/p5@1.4.1/lib/p5.js", "gd.jpg", "style.css"];
 
 async function precache() {
@@ -13,7 +13,7 @@ self.addEventListener("install", (event) => {
 async function cacheFirstWithRefresh(request) {
   const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
     if (networkResponse.ok) {
-      const cache = await caches.open("cache-v1");
+      const cache = await caches.open(cacheName);
       cache.put(request, networkResponse.clone());
     }
     return networkResponse;
