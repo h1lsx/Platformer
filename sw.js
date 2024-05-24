60921693ccf5ae5e5f4bd49e509605d66370f05b
h1lsx <139397050+h1lsx@users.noreply.github.com>
2024-05-24 12:13:02 +0000

    pwa fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cacheName = "cache-v2";
-const precachedResources = ["index.html", "init.js", "script.js", "edit.js", "classes.js", "tiles.png", "gdicons/ball.png", "gdicons/cubeportal.png", "gdicons/autoportal.png", "gdicons/platformerportal.png", "gdicons/icon.png", "gdicons/jetpackl.png", "gdicons/jetpackr.png", "gdicons/wave.png", "https://cdn.jsdelivr.net/npm/p5@1.4.1/lib/p5.js", "gd.jpg", "style.css"];
+const precachedResources = ["index.html", "init.js", "script.js", "edit.js", "classes.js", "tiles.png", "gdicons/ball.png", "gdicons/cubeportal.png", "gdicons/autoportal.png", "gdicons/ballportal.png", "gdicons/waveportal.png", "gdicons/shipportal.png", "gdicons/spiderportal.png", "gdicons/upportal.png", "gdicons/downportal.png", "gdicons/platformerportal.png", "gdicons/icon.png", "gdicons/jetpackl.png", "gdicons/jetpackr.png", "gdicons/wave.png", "https://cdn.jsdelivr.net/npm/p5@1.4.1/lib/p5.js", "gd.jpg", "style.css"];
 
 async function precache() {
   const cache = await caches.open(cacheName);
